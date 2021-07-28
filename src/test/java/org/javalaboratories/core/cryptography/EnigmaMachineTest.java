368bbea37bb0de34aac236722a7abdf70af98f9d
ArcaneMage <kevin.henry@javalaboratories.org>
2021-07-28 13:45:58 +0100

    Reordered unit tests to ensure dependency is assured

--- src/test/java/org/javalaboratories/core/cryptography/EnigmaMachineTest.java
+++ src/test/java/org/javalaboratories/core/cryptography/EnigmaMachineTest.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
+@TestMethodOrder(value = MethodOrderer.Alphanumeric.class)
 public class EnigmaMachineTest {
 
     private Configuration configuration;
@@ -33,8 +33,7 @@ public class EnigmaMachineTest {
     }
 
     @Test
-    @Order(1)
-    public void testExecute_Encryption_Pass() {
+    public void testExecute_A_Encryption_Pass() {
         // Given
         CommandLineArguments arguments = new DefaultCommandLineArguments(configuration);
         arguments.parse(toArgs("-c=src/test/resources/public-certificate-test.cer -f=src/test/resources/text.original.data -e -o=src/test/resources/text.out.enc"));
@@ -48,8 +47,7 @@ public class EnigmaMachineTest {
     }
 
     @Test
-    @Order(2)
-    public void testExecute_Decryption_Pass() {
+    public void testExecute_B_Decryption_Pass() {
         // Given
         CommandLineArguments arguments = new DefaultCommandLineArguments(configuration);
         arguments.parse(toArgs("-v=src/test/resources/keys-vault.jks -p=TESTING -f=src/test/resources/text.out.enc -d -o=src/test/resources/text.out"));
